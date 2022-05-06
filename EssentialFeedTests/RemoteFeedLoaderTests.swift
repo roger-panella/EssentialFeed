4e0224b7124612e2b9843ed916d8c47e8d7dfab1
Roger Panella <rogerpanella@gmail.com>
2022-05-05 20:41:48 -0500

    replace stubbing by capturing completion

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -28,10 +28,13 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
-        client.error = NSError(domain: "Test", code: 0)
         
         var capturedErrors = [RemoteFeedLoader.Error]()
         sut.load { capturedErrors.append($0) }
+        
+        let clientError = NSError(domain: "Test", code: 0)
+        client.completions[0](clientError)
+    
         XCTAssertEqual(capturedErrors, [.connectivity])
     }
     
@@ -46,12 +49,11 @@ class RemoteFeedLoaderTests: XCTestCase {
     private class HTTPClientSpy: HTTPClient {
         var requestedURLs: [URL] = []
         var error: Error?
+        var completions: [(Error) -> Void] = []
         
         func get(from url: URL?, completion: @escaping (Error) -> Void) {
+            completions.append(completion)
             requestedURLs.append(url!)
-            if let error = error {
-                completion(error)
-            }
         }
     }
 
