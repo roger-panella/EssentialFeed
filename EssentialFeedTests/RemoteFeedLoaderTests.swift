26e63946587240c82f92f6e2b2c82403037f222b
Roger Panella <rogerpanella@gmail.com>
2022-05-08 17:57:11 -0500

    Update test from last video

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -40,18 +40,15 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     func test_load_deliversErrorOnNon200HTTPResponse() {
         let (sut, client) = makeSUT()
-
-        var capturedErrors: [RemoteFeedLoader.Error] = []
-        sut.load { capturedErrors.append($0) }
         
         let testStatusCodes = [199, 201, 300, 400, 500]
 
-        testStatusCodes.enumerated().forEach {
-            client.complete(withStatusCode: $1, at: $0)
-            XCTAssertEqual(capturedErrors, [.invalidData])
-            
+        testStatusCodes.enumerated().forEach { index, code in
             var capturedErrors: [RemoteFeedLoader.Error] = []
             sut.load { capturedErrors.append($0) }
+
+            client.complete(withStatusCode: code, at: index)
+            XCTAssertEqual(capturedErrors, [.invalidData])
         }
     }
     
