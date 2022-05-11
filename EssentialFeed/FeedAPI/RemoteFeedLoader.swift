283300401f6b775e53424345aea03140624baa32
Roger Panella <rogerpanella@gmail.com>
2022-05-10 21:33:53 -0500

    Extract response handling to mapper function

--- EssentialFeed/FeedAPI/RemoteFeedLoader.swift
+++ EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -33,8 +33,8 @@ public final class RemoteFeedLoader {
         client.get(from: url) { result in
             switch result {
             case let .success(data, response):
-                if let root = try? JSONDecoder().decode(Root.self, from: data), response.statusCode == 200 {
-                    completion(.success(root.items.map { $0.item }))
+                if let items = try? FeedItemsMapper.map(data, response) {
+                    completion(.success(items))
                 } else {
                     completion(.failure(.invalidData))
                 }
@@ -43,6 +43,15 @@ public final class RemoteFeedLoader {
                 completion(.failure(.connectivity))
             }
         }
+    }
+}
+
+private class FeedItemsMapper {
+    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
+        guard response.statusCode == 200 else {
+            throw RemoteFeedLoader.Error.invalidData
+        }
+        return try JSONDecoder().decode(Root.self, from: data).items.map { $0.item }
     }
 }
 
