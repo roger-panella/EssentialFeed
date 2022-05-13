0a2a52656274a1bde99e02de0744ccbe47c0e5b6
Roger Panella <rogerpanella@gmail.com>
2022-05-12 20:58:12 -0500

    put API types into FeedItemsMapper, small refactor

--- EssentialFeed/FeedAPI/RemoteFeedLoader.swift
+++ EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -30,6 +30,10 @@ public final class RemoteFeedLoader {
     }
 
     public func load(completion: @escaping (Result) -> Void) {
+        let error: RemoteFeedLoader.Error = .invalidData
+        if error == .connectivity {
+            print("")
+        }
         client.get(from: url) { result in
             switch result {
             case let .success(data, response):
@@ -47,25 +51,26 @@ public final class RemoteFeedLoader {
 }
 
 private class FeedItemsMapper {
+    private struct Root: Decodable {
+        let items: [Item]
+    }
+
+    private struct Item: Decodable {
+        let id: UUID
+        let description: String?
+        let location: String?
+        let image: URL
+        
+        var item: FeedItem {
+            return FeedItem(id: id, description: description, location: location, imageURL: image)
+        }
+    }
+
     static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
         guard response.statusCode == 200 else {
             throw RemoteFeedLoader.Error.invalidData
         }
-        return try JSONDecoder().decode(Root.self, from: data).items.map { $0.item }
-    }
-}
-
-private struct Root: Decodable {
-    let items: [Item]
-}
-
-private struct Item: Decodable {
-    let id: UUID
-    let description: String?
-    let location: String?
-    let image: URL
-    
-    var item: FeedItem {
-        return FeedItem(id: id, description: description, location: location, imageURL: image)
+        let root = try JSONDecoder().decode(Root.self, from: data)
+        return root.items.map { $0.item }
     }
 }
