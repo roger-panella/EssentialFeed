1464144b6241f760dafc31358abdb7d46be1eec8
Roger Panella <rogerpanella@gmail.com>
2022-06-07 23:11:03 -0500

    make url session http client conform to HTTPClient protocol

--- EssentialFeedTests/FeedAPI/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/FeedAPI/RemoteFeedLoaderTests.swift
@@ -165,8 +165,8 @@ class RemoteFeedLoaderTests: XCTestCase {
             return messages.map { $0.url }
         }
         
-        func get(from url: URL?, completion: @escaping (HTTPClientResult) -> Void) {
-            messages.append((url!, completion))
+        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+            messages.append((url, completion))
         }
         
         func complete(with error: Error, at index: Int = 0) {
